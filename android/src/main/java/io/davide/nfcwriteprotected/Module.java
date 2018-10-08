0393d1b670218222c2232a1578d38ce74b355640
Davide Cascone <d.cascone@greenvulcano.com>
2018-10-08 14:55:49 +0200

    NFC connect function with TapLinx SDK

--- android/src/main/java/io/davide/nfcwriteprotected/Module.java
+++ android/src/main/java/io/davide/nfcwriteprotected/Module.java
@@ -5,14 +5,26 @@ import android.widget.Toast;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import com.nxp.nfclib.desfire.IDESFireEV1;
+import com.nxp.nfclib.icode.ICodeFactory;
+import com.nxp.nfclib.icode.IICodeSLIXS;
+import com.nxp.nfclib.ntag.INTag213215216;
+import com.nxp.nfclib.ntag.NTagFactory;
+import com.nxp.nfclib.utils.Utilities;
+import com.nxp.nfclib.CardType;
+import com.nxp.nfclib.NxpNfcLib;
+
 
 public class Module extends ReactContextBaseJavaModule {
 
   private static final String DURATION_SHORT_KEY = "SHORT";
   private static final String DURATION_LONG_KEY = "LONG";
+
+  private NxpNfcLib nxpLib = RNProtectedWritePackage.getLibInstance();
+
 
   public Module(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -32,7 +44,25 @@ public class Module extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void show(String message, int duration) {
-    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  public void show(String message) {
+
+    Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_SHORT).show();
+  }
+
+
+  @ReactMethod
+  public void connect() {
+
+    try{
+      INTag213215216 objNtag = NTagFactory.getInstance().getNTAG213(RNProtectedWritePackage.getLibInstance().getCustomModules());
+      objNtag.getReader().connect();
+
+    } catch (Exception e) {
+
+      Toast.makeText(getReactApplicationContext(), "NFC connect error", Toast.LENGTH_LONG).show();
+    }
+
+
+    Toast.makeText(getReactApplicationContext(), "NFC connected", Toast.LENGTH_LONG).show();
   }
 }
--- android/src/main/java/io/davide/nfcwriteprotected/RNProtectedWritePackage.java
+++ android/src/main/java/io/davide/nfcwriteprotected/RNProtectedWritePackage.java
@@ -5,13 +5,36 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import com.nxp.nfclib.NxpNfcLib;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import android.app.Activity;
+
 public class RNProtectedWritePackage implements ReactPackage {
 
+    private Activity mActivity = null;
+
+    private static NxpNfcLib nxpLib = null;
+    private String nxpLibKey = "ca5b8b55afc1d1267faf36fc14bc9ac3";
+
+
+    public RNProtectedWritePackage(Activity activity) {
+        mActivity = activity;
+    }
+
+    private void initializeLibrary()
+    {                                        // Initialize the TapLinx library
+        nxpLib = NxpNfcLib.getInstance();
+        nxpLib.registerActivity(mActivity, nxpLibKey);
+    }
+
+    public static NxpNfcLib getLibInstance() {
+
+        return nxpLib;
+    }
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
