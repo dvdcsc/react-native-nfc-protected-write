441f96c974d48301e849dfa20f2445d7915958df
Davide Cascone <d.cascone@greenvulcano.com>
2018-10-24 18:21:09 +0200

    asd

--- android/src/main/java/io/davide/nfcwriteprotected/Module.java
+++ android/src/main/java/io/davide/nfcwriteprotected/Module.java
@@ -143,10 +143,10 @@ public class Module extends ReactContextBaseJavaModule  implements ActivityEvent
       byte[] data = objNtag.read(0x0f);
       Toast.makeText(getReactApplicationContext(), "read page 0x0f: " + Utilities.byteToHexString(data) , Toast.LENGTH_LONG).show();
       objNtag.write(0x0f, byToWrite);
-      objNtag.enablePasswordProtection(true,0x0f);
-      Toast.makeText(getReactApplicationContext(), "enablePasswordProtection: " , Toast.LENGTH_LONG).show();
-      data = objNtag.read(0x10);
-      Toast.makeText(getReactApplicationContext(), "read page 0x10: " + Utilities.byteToHexString(data) , Toast.LENGTH_LONG).show();
+      //objNtag.enablePasswordProtection(true,0x0f);
+      //Toast.makeText(getReactApplicationContext(), "enablePasswordProtection: " , Toast.LENGTH_LONG).show();
+      data = objNtag.read(0x0f);
+      Toast.makeText(getReactApplicationContext(), "read page 0x0f: " + Utilities.byteToHexString(data) , Toast.LENGTH_LONG).show();
 
 
       //objNtag.write(0x10, byToWrite);
